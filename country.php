74c325d4ac39b6f375b552443b0cf582309890b6
jackuson14 <45443578+jackuson14@users.noreply.github.com>
2019-04-16 16:30:39 +0800

    Add files via upload

--- country.php
+++ country.php
@@ -70,7 +70,6 @@
 		   background-color: #588c7e;
 		   color: white;
 			}
-		  tr:nth-child(even) {background-color: #f2f2f2}
 		  
 		  body {
 			  background-color: beige;
@@ -126,6 +125,92 @@
 				text-align: left;
 			}
 			}
+
+			table{
+			width:100%;
+			table-layout: fixed;
+			}
+			.tbl-header{
+			background-color: rgba(255,255,255,0.3);
+			}
+			.tbl-content{
+			height:300px;
+			overflow-x:auto;
+			margin-top: 0px;
+			border: 1px solid rgba(255,255,255,0.3);
+			}
+			th{
+			padding: 20px 15px;
+			text-align: left;
+			font-weight: 500;
+			font-size: 12px;
+			color: #fff;
+			text-transform: uppercase;
+			}
+			td{
+			padding: 15px;
+			text-align: left;
+			vertical-align:middle;
+			font-weight: 300;
+			font-size: 12px;
+			color: #fff;
+			border-bottom: solid 1px rgba(255,255,255,0.1);
+			}
+
+
+			/* demo styles */
+
+			@import url(https://fonts.googleapis.com/css?family=Roboto:400,500,300,700);
+			body{
+			background: -webkit-linear-gradient(left, #25c481, #25b7c4);
+			background: linear-gradient(to right, #25c481, #25b7c4);
+			font-family: 'Roboto', sans-serif;
+			}
+			section{
+			margin: 50px;
+			}
+
+
+			/* follow me template */
+			.made-with-love {
+			margin-top: 40px;
+			padding: 10px;
+			clear: left;
+			text-align: center;
+			font-size: 10px;
+			font-family: arial;
+			color: #fff;
+			}
+			.made-with-love i {
+			font-style: normal;
+			color: #F50057;
+			font-size: 14px;
+			position: relative;
+			top: 2px;
+			}
+			.made-with-love a {
+			color: #fff;
+			text-decoration: none;
+			}
+			.made-with-love a:hover {
+			text-decoration: underline;
+			}
+
+
+			/* for custom scrollbar for webkit browser*/
+
+			::-webkit-scrollbar {
+				width: 6px;
+			} 
+			::-webkit-scrollbar-track {
+				-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
+			} 
+			::-webkit-scrollbar-thumb {
+				-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
+			}
+			.wrapper {
+				text-align: right;
+			}
 	</style>
 
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
@@ -152,6 +237,13 @@
 			}
 			}
 			</script>
+			
+			<script>
+			$(window).on("load resize ", function() {
+			var scrollWidth = $('.tbl-content').width() - $('.tbl-content table').width();
+				$('.tbl-header').css({'padding-right':scrollWidth});
+			}).resize();
+			</script>
 
 	</head>
 	<body>
@@ -169,14 +261,18 @@
                             </div>
                         <button type="submit" class="btn btn-primary">Search</button>
                     </form>
+					
                 </div>
+				<form action = "insertCountry.php" method ="post">
+					<button type="submit" name =addbutton class="btn btn-primary">Add Country fields</button>
+				</form>
             </div>
 		</div>
 		<p></p>
-		<p></p>				 
-	</body>
-</html>
-
+		<p></p>	
+		<div style="overflow-x:auto;">
+			<div class="tbl-content">
+			<table cellpadding="0" cellspacing="0" border="0">
 			<?php
 				//SELECT
 						
@@ -251,3 +347,12 @@
 										
 					$conn-> close();
 			?>
+			</div>
+	</table>
+	</div>			 
+	</body>
+	<div class="made-with-love">
+	<i>World Datasets 2019</i>
+	</div>
+</html>
+			
